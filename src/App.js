83c4b80278876542b4d3dfb2539fd79ca59e54e8
Ashish Rawat <rashish@thoughtworks.com>
2021-07-02 08:40:34 +0530

    Ashish | Stage 2 - Display File System - add collapse-expand feature

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { backendData } from './fetchData'
 
+const FileDisplayView = ({ file, children }) => {
+  const [expanded, setExpanded] = useState(true)
+  console.log('rendering !!!')
+  const handleClick = event => {
+    setExpanded(!expanded)
+    event.stopPropagation()
+  }
+
+  return (
+    <div className="parent-view" onClick={handleClick}>
+      <h4 key={file.id}>{file.name}</h4>
+      {expanded && children}
+    </div>
+  )
+}
+
 const DisplayFiles = ({ filesData }) => {
   function getFileView(file) {
     return (
-      <div className="parent-view">
-        <h4 key={file.id}>{file.name}</h4>
+      <FileDisplayView file={file}>
         {file.children && file.children.length && <div>{file.children.map(child => getFileView(child))}</div>}
-      </div>
+      </FileDisplayView>
     )
   }
 
